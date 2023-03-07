2dc556d6eac2b55dec16f3dd0b287e69bf0e9629
Rajnandanikumari <118913721+Rajnandanikumari@users.noreply.github.com>
2023-03-07 23:40:03 +0530

    Add files via upload

---
+++ uttam15.c
@@ -0,0 +1,210 @@
+#include<stdio.h>
+int gretest number(int x);
+int main()
+{
+    max=greatest number(x);
+}
+int greatest number(int x)
+{
+    int max,i,j;
+    for(i=1;i<=5;i++)
+    {
+        for(j=i+1;j<=5;j++)
+            if(max<a[i])
+            max=a[i]
+        printf("%d\n",a[i]);
+        return 0;
+    }
+
+    #include<stdio.h>
+    int smallest number(int x);
+    int main()
+    {
+        min=smallest number(x);
+    }
+    int smallest number(int x)
+    {
+        int i,j,n,min;
+        printf("Enter 10 number\n");
+        for(i=0;i<=5;i++)
+        {
+            scanf("%d",&a[i]);
+        }
+        for(j=i+1;j<=5;j++)
+        {
+            if(a[i]<min)
+                a[i]min;
+            printf("%d\n",a[i]);
+            return 0;
+        }
+
+        #include<stdio.h
+        void sort(int x)
+        int main()
+        {
+            sort array(int x)
+        }
+        void sort(int x)
+        {
+            int a[]={8,9,34,76,23,45,36},i,j,t;
+            for(i=0;i<6;i++)
+            {
+                for(j=0;j<6;j++)
+                {
+                    if(a[i]>a[j])
+                    {
+                        t=a[i];
+                        a[i]=a[j];
+                        a[j]=t;
+                    }
+                }
+            }
+            for(i=0;i<7;i++)
+            {
+                printf("%d",a[i]);
+                return o;
+            }
+
+            #include<stdio.h>
+            void rightRotation(int a[],int size)
+            {
+                int i,j,x;
+                for(i=1;i<=d;i++)
+                {
+                    x=a[size -1];
+                    for(j=size-1;j>0;j--)
+                    {
+                        a[j]=a[j-1];
+                    }
+                    a[0]=x;
+                }
+                for(i=0;i<size;i++)
+                {
+
+                    printf("%d",a[i]);
+                }
+                printf("d",a[i]);
+            }
+            print("\n\n");
+        }
+        int main()
+        {
+            int a[]={1,2,3,4,5,6,7,8,9};
+            rightRotation(a,9,2);
+            return 0;
+        }
+
+        #include<stdio.h>
+        int main()
+        {
+            int a[]={1,3,4,5,9,2,3,5,9,1,4};
+            int hash[100]={0},i;
+            for(i=0;i<11;i++)
+            hash[[a]++;
+            for(i=0;i<11;i++)
+            {
+                if(hash[i]!=0)
+                {
+                    printf("%d---->%d\n",i,hash[i]);
+                }
+            }
+            return 0;
+                }
+
+                #include<stdio.h>
+                int main()
+                {
+
+                    int a[]={1,2,3,5,2,4,6,8,5,3,6,7};
+                    int hash[10]={0},i;
+                    for(i=0;i<12;i++)
+                    {
+                        hash[[a]]++;
+                    }
+                    for(i=0;i<10;i++)
+                    {
+                        if(hash[i]==2)
+                            printf("%d",i);
+                    }
+                    return 0;
+                    }
+
+                    #include<stdio.h>
+                    int main()
+                    {
+                        int a[]={1,2,3,4,5,2,4,6,8,5,3,6,7};
+                        int hash[10]={0},i;
+                        for(i=0;i<12;i++)
+                        {
+                            hash[a[i]]++;
+                        }
+                        for(i=0;i<10;i++)
+                        {
+                            if(hash[i]==1)
+                                printf("%d",i);
+                        }
+                        return 0;
+                        }
+
+                        #include<stdio.h>
+                        int main()
+                        {
+
+                        int a[]={2,4,6,9,10};
+                        int b[]={1,5,8,10,11};
+                        int i,j;
+                        for(i=0;j=0;i<5&&j<5);
+                        {
+                            if(a[i]<b[j])
+                            {
+                                c[k]=a[i];
+                                i++;
+                            }
+                            else
+                            {
+                                c[k]=b[j];
+                                j++;
+                            }
+                        }
+                        if(i==5&&j!=5)
+                        {
+                            for(j=0;j<5;j++)
+                                c[k++]=b[j];
+                        }
+                        if(i!=5&&j==5)
+                        {
+
+                        }
+                        }
+                            }
+
+                            }
+
+                            }
+                        }
+                        }
+                        }
+                    }
+                    }
+                }
+            }
+        }
+        }
+                }
+                    }
+                }
+            }
+            {
+                int
+            }
+
+                    }
+                }
+            }
+
+        }
+        }
+    }
+    }
+}
+}
