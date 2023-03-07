544e41394b05a4552005c0032bdba3c04958dba8
Rajnandanikumari <118913721+Rajnandanikumari@users.noreply.github.com>
2023-03-07 23:58:25 +0530

    Add files via upload

---
+++ uttam17.c
@@ -0,0 +1,147 @@
+#include<stdio.h>
+int main()
+{
+    char a[10]={'a'.'b','c','d','\0'};
+    char name2[]="jain";
+    int i,length=0;
+    printf("%s%s",name,name2);
+    for(i=0;name[i]!='\0';i++)
+    {
+        length++;
+    }
+    printf("length os string =%d\n",length);
+    return 0;
+    }
+
+    #include<stdio.h>
+    int main()
+
+    {
+        a[10]={'a','b','c','d','\0'};
+        int i,count=0;
+        printf("%s%s",name);
+        for(i=0;name[i]='\0',i++)
+        {
+            count++;
+
+        }
+        printf("occure of character=%d\n",count);
+        return 0;
+    }
+
+    #include<stdio.h>
+    int main()
+    {
+        char a[10]="raj";
+        int i=0;
+        while(a[i]='\0')
+        {
+            if((a[i]>='a')&&(a[i]<='z'))
+                a[i]=a[i]+a;
+            printf("\n Now string is %s\n",a);
+            return 0;
+        }
+        #include<stdio.h>
+        int main()
+        {
+
+            char a[10]="Raj";
+            int i=0;
+            while(a[i]!='\0')
+            {
+                if((a[i]>=A)&&(a[i]<'z'))
+                    a[i]=a[i]+a;
+                printf("\n Now string is %s\n",a);
+                return 0;
+            }
+
+            #include<stdio.h>
+            int main()
+            {
+                char name[]="prateek";
+                int length=0;i=0;
+                while(name[i]!='\0')
+                {
+                    length++;
+                    i++;
+                }
+                printf("length of string=%d\n",length);
+                for(i=length;i>=0;i--)
+                    printf("%c",name[i]);
+                return 0;
+            }
+
+            #include<stdio.h>
+            int main()
+            {
+                char name[]="shyam145@gmail.com";
+                int length=0,i=0;
+                int digitcount=0;Alphcount=0,specialcount=0;
+                while(name[i]!='\0')
+                {
+                    length++;
+                    if(name[i]>='0'&&name[i]<='z')
+                        Alphacount++;
+                    else if(name[i]>='0'&&name[i]<='9')
+                        digitcount++;
+                    else
+                        specialcount++;
+                    i++;
+                }
+                printf("Digit count=%d,Alpha=%d,special=%d",digitcount,Alphacount,specialcount);
+                return 0;
+                }
+
+                #include<stdio.h>
+                int main()
+                {
+                    char a[10]="Raj,b[10];
+                    int i=0;
+                    while(a[i]!='\0')
+                    {
+                       b[i]=a[i];
+                       i++;
+                    }
+                    b[i]='\0'
+                    printf("\ncopied string is %s\n",b);
+                    return 0;
+                    }
+
+                    #include<stdio.h>
+                    int main()
+                    {
+                        int a[]={'1','3','5','4'};
+                        int i=0,min=0;
+                        if(min<a[i])
+                        min=a[i];
+                        print("%d",a[i]);
+                        return 0;
+                    }
+
+                    #include<stdio.h>
+                    int main()
+                    {
+                        char a[]={'a','c','d','f'};
+                        int i,frequency=0;
+                        printf("%s%s",name);
+                        for(i=0; name[i]!='\0';i++)
+{
+    printf("frequency of character=%d\n",char)
+                   return 0;
+}
+
+
+                    }
+
+            }
+
+                }
+            }
+
+            }
+        }
+
+        }
+    }
+
+}
